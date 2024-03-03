b6e5d3ac6accec3694e66f598c745cae50190a75
El walid bousmaha <elwalid.bousmaha@outlook.com>
2024-03-03 17:59:50 +0100

    added to footer playstore and appstore badge

--- app.py
+++ app.py
@@ -64,5 +64,9 @@ def logout():
 def search():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
